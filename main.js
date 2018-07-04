a6d7b0b8b2a87ea9ecfaed637bffd0cc0fd4fb60
Julien Turby <julien.turby@hellocasa.fr>
2018-07-04 22:25:29 +0200

    update js

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var currentError = document.getElementsByClassName("break-long-words trace-message")[0];
-    if (currentError) {
-        var content = currentError.innerHTML;
-        currentError.innerHTML += "<h3>Solve your problem</h3>";
-        currentError.innerHTML += "<a target='_blank' href='https://google.com/search?q="+content+"'>Google</a>";
-        currentError.innerHTML += "<br>";
-        currentError.innerHTML += "<a target='_blank' href='https://stackoverflow.com/search?q="+content+"'>Stackoverflow</a>";
+function displayLinks(classError) {
+    var currentError = document.getElementsByClassName(classError)[0];
+    if (!currentError) {
+        return;
     }
+
+    var content = currentError.innerHTML;
+    currentError.innerHTML += "<h3>Solve your problem</h3>";
+    currentError.innerHTML += "<a target='_blank' href='https://google.com/search?q="+content+"'>Google</a>";
+    currentError.innerHTML += "<br>";
+    currentError.innerHTML += "<a target='_blank' href='https://stackoverflow.com/search?q="+content+"'>Stackoverflow</a>";
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    displayLinks('break-long-words trace-message');
+    displayLinks('break-long-words exception-message');
 }, false);
