fb83eadfbbe1a87f8355dfae38b4f32e496d83b6
Julien Turby <julien.turby@hellocasa.fr>
2018-07-04 14:09:44 +0200

    change condition

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var currentError = document.getElementsByClassName("break-long-words trace-message");
+    var currentError = document.getElementsByClassName("break-long-words trace-message")[0];
     if (currentError) {
-        var content = currentError[0].innerHTML;
-        currentError[0].innerHTML += "<h3>Solve your problem</h3>";
-        currentError[0].innerHTML += "<a target='_blank' href='https://google.com/search?q="+content+"'>Google</a>";
-        currentError[0].innerHTML += "<br>";
-        currentError[0].innerHTML += "<a target='_blank' href='https://stackoverflow.com/search?q="+content+"'>Stackoverflow</a>";
+        var content = currentError.innerHTML;
+        currentError.innerHTML += "<h3>Solve your problem</h3>";
+        currentError.innerHTML += "<a target='_blank' href='https://google.com/search?q="+content+"'>Google</a>";
+        currentError.innerHTML += "<br>";
+        currentError.innerHTML += "<a target='_blank' href='https://stackoverflow.com/search?q="+content+"'>Stackoverflow</a>";
     }
 }, false);
